2bc292c9be058706336cb663009927626b57c1e9
Dima <125818471+DmitriyKislyakov@users.noreply.github.com>
2024-08-01 00:23:50 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,8 +10,46 @@ function createGrid(cellCount) {
       let cell = document.createElement('div')
       cell.classList.add('cell')
       row.appendChild(cell)
+      cell.addEventListener('mousemove', (e) => {
+        e.target.style.background = 'gray'
+      })
     }
   }
 }
 
 createGrid(cellCount)
+
+function resetFunction() {
+  let cells = document.querySelectorAll('.cell')
+  cells.forEach((cell) => {
+    cell.style.background = 'whitesmoke'
+  })
+}
+
+const reset = document.querySelector('#reset')
+reset.addEventListener('click', resetFunction)
+
+function trueNumber(str) {
+  let num = Number(str)
+  if ((num ^ 0) === num) {
+    return num >= 2 && num <= 100
+  }
+  return false
+}
+
+function resizeFunction() {
+  let number = prompt(
+    'Choose Your cell-count on side\n(Cell-count must be integer and in[2; 100]):'
+  )
+  if (trueNumber(number)) {
+    cellCount = Number(number)
+    container.innerHTML = ''
+    return createGrid(cellCount)
+  } else {
+    alert('Incorrect number! Repeat pleace.')
+    return resizeFunction()
+  }
+}
+
+const resize = document.querySelector('#resize')
+resize.addEventListener('click', resizeFunction)
